4792e5161809b425c27cbd5ef11828285d07a633
Craig Marais <craig.marais.85@gmail.com>
2024-01-22 10:05:08 +0100

    feat: Added CountTo

--- Assignement_1/Program.cs
+++ Assignement_1/Program.cs
@@ -6,5 +6,16 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("My name is Craig");
+        
+        CountTo(4);
+    }
+
+    static void CountTo(int num)
+    {
+        Console.WriteLine("Counting to "+ num + "...");
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
